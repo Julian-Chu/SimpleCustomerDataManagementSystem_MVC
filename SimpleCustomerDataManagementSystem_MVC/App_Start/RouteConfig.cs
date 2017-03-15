1b27662b2bfc174d06d8dc34c1162fafe941323f
JulianChu <yulang.chu@gmail.com>
2017-03-16 02:54:44 +0800

    - 更改Route直接連結至Login頁面 - 在_Layout.cshtml註解掉多餘連結

--- SimpleCustomerDataManagementSystem_MVC/App_Start/RouteConfig.cs
+++ SimpleCustomerDataManagementSystem_MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace SimpleCustomerDataManagementSystem_MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "客戶資料統計", action = "Index", id = UrlParameter.Optional }
+
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
